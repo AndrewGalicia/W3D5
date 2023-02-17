fb19125f67de03ec67072dc1d3bc90da735604fc
Andrew Galicia <andrewlgalicia@gmail.com>
2023-02-17 14:42:12 -0800

    update

--- W3D5/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ W3D5/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,20 @@
+
+
 class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    
+    end
+
+    def parent=(new_parent)
+        @parent = new_parent
+        if !new_parent.children.include?(self)
+            new_parent.children.push(self)
+        end
+    end
   
 end
