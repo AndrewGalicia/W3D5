8a23dcec3e647fc89952a1a14ada43ee492ceedf
mchung099 <mchung099@gmail.com>
2023-02-20 14:17:12 -0800

    added self.valid_moves

--- W3D5/knights_travails/knight_path_finder.rb
+++ W3D5/knights_travails/knight_path_finder.rb
@@ -1,10 +1,13 @@
 require_relative "tree_node.rb"
+require "byebug"
 
 class KnightPathFinder
   MOVES = [
-    [-2,-1]
+    [2,1], [2,-1], [-2,1], [-2,-1], [1,2], [1,-2], [-1,2], [-1,-2]
   ]
 
+  # [row+2, col+1], [row+2, col-1] [row-2, col+1], [row-2, col-1]
+  # [row+1, col-2] [row-1, col+2] [row-1, col-2], [row+1, col+2]
 
   def self.root_node(pos)
     row, col = pos
@@ -13,18 +16,20 @@ class KnightPathFinder
 
   #from 0 to 7
   #moves are all possible directions like a slope on a graph
-  
+
   def self.valid_moves(pos)
+    row, col = pos
     valids = []
+    MOVES.each_with_index do |x, i|
+      valids << [pos[0].to_i + MOVES[i][0], pos[1].to_i + MOVES[i][1]]
+    end
 
+    valids.select do |pos|
+      x = pos[0]
+      y = pos[1]
+      (x >= 0 && x < 8) && (y >= 0 && y < 8)
+    end
   end
-
-  #[0,0] [2,1] []
-
-  #[4,4] =>
-  # [row+2, col+1], [row+2, col-1] [row-2, col+1], [row-2, col-1]
-  # [row+1, col-2] [row-1, col+2] [row-1, col-2], [row+1, col+2]
-  # [row-1, col-2]
 
   def initialize(pos)
     initial_pos = KnightPathFinder.root_node(pos)
@@ -37,10 +42,13 @@ class KnightPathFinder
 
       pos
       @considered_positions << pos
+    end
   end
 
 end
 
 if $PROGRAM_NAME == __FILE__
   p KnightPathFinder.root_node([0,0])
+  p KnightPathFinder.valid_moves([0,0])
+  p KnightPathFinder.valid_moves([7,7])
 end
