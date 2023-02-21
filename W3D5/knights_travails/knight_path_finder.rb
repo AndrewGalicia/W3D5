52fcfd41cf1503530547ed03cf6769f69a3a586a
Andrew Galicia <andrewlgalicia@gmail.com>
2023-02-20 17:44:26 -0800

    find path

--- W3D5/knights_travails/knight_path_finder.rb
+++ W3D5/knights_travails/knight_path_finder.rb
@@ -29,9 +29,15 @@ class KnightPathFinder
 
   def initialize(pos)
     @initial_pos = pos
-    @considered_positions = [initial_pos]
+    @considered_positions = [@initial_pos]
     @root_node = PolyTreeNode.new(pos)
+    @tree = self.build_move_tree
   end
+
+  def root_node
+    @root_node
+  end
+
 
   def considered_positions
     @considered_positions
@@ -57,8 +63,39 @@ class KnightPathFinder
         nodes << new_node #[[2,1], [1,2]]
       end
     end
+  end
+
+  #we are searching for the end position,
+  #use .dfs(end_pos) or .bfs(end_pos)
+  #returning the tree node instance containing end position. looking for a node.value == end_position
+  def find_path(end_pos)
+
+    end_node = @root_node.dfs(end_pos)
+    trace_path_back(end_node)
+  end
+
+  def trace_path_back(end_node)
+
+    node_path = []
+    until end_node.parent.nil?
+      node_path.unshift(end_node.parent)
+    end
+
+    return node_path
 
   end
+
+#   def parent=(new_parent)
+#     if @parent
+#       @parent.children.delete(self)
+#     end
+
+#     @parent = new_parent
+
+#     if @parent != nil
+#         new_parent.children.push(self)
+#     end
+# end
 
 end
 
@@ -66,7 +103,47 @@ if $PROGRAM_NAME == __FILE__
   # p KnightPathFinder.root_node([0,0])
   # p KnightPathFinder.valid_moves([0,0])
   # p KnightPathFinder.valid_moves([7,7])
-  test = KnightPathFinder.new([0,0])
-  p test.new_move_positions([7,7])
-  p test.considered_positions
+
+  # p test.new_move_positions([7,7])
+  # p test.considered_positions
+#   test = KnightPathFinder.new([0,0])
+#  test.new_move_positions([0,0])
+#   # print test.build_move_tree
+#   puts test.find_path([2,1])
+print kpf = KnightPathFinder.new([0, 0])
+print kpf.find_path([7, 6]) 
+puts kpf.find_path([6, 2])
 end
+
+
+# Knight's Travails Continued (Part 2)
+
+# **Finish Phases I & II before continuing!**
+
+# ## Phase III: `#find_path`
+
+# Now that you have created your internal data structure (the move tree stored in
+# `self.root_node`), you can traverse it to find the shortest path to any position
+# on the board from your original `@start_pos`.
+
+# Create an instance method `#find_path(end_pos)` to search for `end_pos` in the
+# move tree. You can use either `dfs` or `bfs` search methods from the
+# `PolyTreeNode` exercises; it doesn't matter. This should return the tree node
+# instance containing `end_pos`.
+
+# This gives you a node, but not a path. Lastly, add a method `#trace_path_back`
+# to `KnightPathFinder`. This should trace back from the node to the root using
+# `PolyTreeNode#parent`. As it goes up-and-up toward the root, it should add each
+# value to an array. `#trace_path_back` should **return** the values in order from
+# the start position to the end position.
+
+# Use `#trace_path_back` to finish up `#find_path`.
+
+# Here are some example paths that you can use for testing purposes. (Your
+# sequences might differ but should be the same number of steps.)
+
+# ```ruby
+# kpf = KnightPathFinder.new([0, 0])
+# kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+# kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
+# ```
